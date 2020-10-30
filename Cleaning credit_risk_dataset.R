6c64c85a585a6239d10c9665e043986c3ec69f60
Gannon Condon <gcondon@sandiego.edu>
2020-10-30 16:18:13 -0700

    Added some notes and visualization

--- Cleaning credit_risk_dataset.R
+++ Cleaning credit_risk_dataset.R
@@ -1,4 +1,7 @@
 library(readr)
+library(ggplot2)
+library(dplyr)
+
 credit_risk_df <- read_csv("credit_risk_dataset (For Cleaning).csv")
 View(credit_risk_dataset_For_Cleaning_)
 
@@ -8,13 +11,24 @@ credit_risk_df$person_home_ownership <- as.factor(credit_risk_df$person_home_own
 credit_risk_df$loan_intent <- as.factor(credit_risk_df$loan_intent)
 credit_risk_df$loan_grade <- as.factor(credit_risk_df$loan_grade)
 credit_risk_df$cb_person_default_on_file <- as.factor(credit_risk_df$cb_person_default_on_file)
+credit_risk_df$loan_status <- as.factor(credit_risk_df$loan_status)
+
+glimpse(credit_risk_df)
 
 # NOTES on Summary stats:
 # person_age: Max = 144
 # person_emp_length: Max = 123; NAs=895
 # loan_int_rate: NAs = 3116
-# Default Rate: 5745/(26836+5745) = 0.1763298
+# Default Rate: 7108/(7108+25473) = 0.218164
+# Total % NAs: (895 + 3116) / (32,581 * 12) = 0.01025905
 
+ggplot(credit_risk_df, aes(person_age, loan_amnt ,color = loan_status)) + 
+  geom_point()
 
+ggplot(credit_risk_df, aes(person_age, loan_int_rate)) + 
+  geom_point()
 
-  
+ggplot(credit_risk_df, aes(person_age, fill = loan_status)) + 
+  geom_histogram(stat = "count") +
+  facet_wrap(~loan_grade)
+cor(credit_risk_df)  
