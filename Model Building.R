4e72a184762f2fdb56430bac76906d85d2f2014f
Gannon Condon <gcondon@sandiego.edu>
2020-10-31 18:06:53 -0700

    Added loop threshold loop script

--- Model Building.R
+++ Model Building.R
@@ -16,8 +16,36 @@ trainIndex <- createDataPartition(credit_risk_df$loan_status, p = .7,
 Train <- credit_risk_df[ trainIndex,]
 Valid <- credit_risk_df[-trainIndex,]
 
-M_LOG<-glm(loan_status ~., data = Train, family = "binomial")
+##First Logistic Regression with all variables
+M_LOG<-glm(loan_status ~., data = Train, family = "binomial", na.action = na.omit)
 summary(M_LOG)
 exp(cbind(M_LOG$coefficients, confint(M_LOG)))
-confusionMatrix(table(predict(M_LOG, Valid, type="response") >= 0.5,
+
+##Loop through threshhold values and print out accuracy of each
+p = .1
+while (p < 1){
+  cm <- confusionMatrix(table(predict(M_LOG, Valid, type="response") >= p,
+                              Valid$loan_status == 1), positive='TRUE')
+  overall.accuracy <- cm$overall['Accuracy']
+  print(overall.accuracy)
+  p <- p + .1
+}
+
+
+##Second logistic Regression
+M_LOG2<-glm(loan_status ~. -cb_person_cred_hist_length -cb_person_default_on_file -person_age, data = Train, family = "binomial", na.action = na.omit)
+summary(M_LOG2)
+exp(cbind(M_LOG$coefficients, confint(M_LOG2)))
+confusionMatrix(table(predict(M_LOG2, Valid, type="response") >= .1,
                       Valid$loan_status == 1), positive='TRUE')
+
+p = .1
+while (p < 1){
+  cm2 <- confusionMatrix(table(predict(M_LOG2, Valid, type="response") >= p,
+                              Valid$loan_status == 1), positive='TRUE')
+  byclass.minfalsepos <- cm2$byClass['Pos Pred Value']
+  print(byclass.minfalsepos)
+  p <- p + .1
+}
+
+
