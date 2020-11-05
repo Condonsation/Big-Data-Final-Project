d8f63699e237d931f49c45fa517a8a3b6a0f95ea
Gannon Condon <gcondon@sandiego.edu>
2020-11-05 12:14:00 -0800

    Worked on ROC curve and working on CART model

--- Model Building.R
+++ Model Building.R
@@ -25,23 +25,41 @@ Valid2 <- fiftydf2[-trainIndex2,]
 
 
 ##Test linear regression with all vars. No split yet
-TrainModel1 <- lm(log(person_income) ~. -cb_person_default_on_file -cb_person_cred_hist_length -loan_percent_income -loan_int_rate, data = Train, na.action = na.omit)
+TrainModel1 <- lm(log(person_income) ~. -cb_person_cred_hist_length -cb_person_default_on_file -loan_percent_income -loan_int_rate, data = Train, na.action = na.omit)
 summary(TrainModel1)
-ValidModel1 <- lm(log(person_income) ~. -cb_person_default_on_file -cb_person_cred_hist_length -loan_percent_income -loan_int_rate, data = Valid, na.action = na.omit)
+par(mfrow=c(2,2))
+plot(TrainModel1)
+# Test for Multicollinearity 
+car::vif(TrainModel1)
+
+##Test against Valid set
+ValidModel1 <- TrainModel1 <- lm(log(person_income) ~. -cb_person_cred_hist_length -cb_person_default_on_file -loan_percent_income -loan_int_rate, data = Valid, na.action = na.omit)
 summary(ValidModel1)
+car::vif(ValidModel1)
 
 ##Linear regression with age as ind. var
-TrainModel2 <- lm(person_age ~. -cb_person_default_on_file -person_income + log(person_income) -loan_percent_income -loan_amnt, data = Train, na.action = na.omit)
+TrainModel2 <- lm(person_age ~.-cb_person_default_on_file -loan_percent_income +log(person_income) -person_income -loan_amnt, data = Train, na.action = na.omit)
 summary(TrainModel2)
 ##RESIDUAL ANALYSIS##
+par(mfrow=c(2,2))
+plot(TrainModel2)
+car::vif(TrainModel2)
+
 plot(TrainModel2$residuals)
 abline(0,0,col='black')
 hist(TrainModel2$residuals)
 summary(TrainModel2$residuals)
 sd(TrainModel2$residuals)
 
-ValidModel2 <- lm(person_age ~. -cb_person_default_on_file -person_income + log(person_income) -loan_percent_income -loan_amnt, data = Valid, na.action = na.omit)
+##Test against Valid set
+ValidModel2 <- lm(person_age ~.-cb_person_default_on_file -loan_percent_income +log(person_income) -person_income -loan_amnt, data = Valid, na.action = na.omit)
 summary(ValidModel2)
+plot(ValidModel2$residuals)
+##View predictions against actual
+predictions <- TrainModel2 %>% predict(Valid)
+View(cbind(Valid$person_age,predictions))
+
+
 ##RESIDUAL ANALYSIS##
 plot(ValidModel2$residuals)
 abline(0,0,col='black')
@@ -53,7 +71,7 @@ sqrt(mean(ValidModel2$residuals^2))
 
 ####
 ##First Logistic Regression
-M_LOG<-glm(loan_status ~. -loan_percent_income -person_income -cb_person_default_on_file -cb_person_cred_hist_length -person_age , data = Train2, family = "binomial", na.action = na.omit)
+M_LOG<-glm(loan_status ~. -loan_percent_income -person_income -cb_person_default_on_file -cb_person_cred_hist_length -person_age , data = Train, family = "binomial", na.action = na.omit)
 summary(M_LOG)
 exp(cbind(M_LOG$coefficients, confint(M_LOG)))
 
@@ -94,27 +112,40 @@ while (p < 1){
 ##Second logistic Regression
 M_LOG2<-glm(loan_status ~. -loan_percent_income, data = Train, family = "binomial", na.action = na.omit)
 summary(M_LOG2)
-exp(cbind(M_LOG$coefficients, confint(M_LOG2)))
-confusionMatrix(table(predict(M_LOG2, Valid, type="response") >= .1,
-                      Valid$loan_status == 1), positive='TRUE')
+exp(cbind(M_LOG2$coefficients, confint(M_LOG2)))
+confusionMatrix(table(predict(M_LOG2, Train, type="response") >= .4,
+                      Train$loan_status == 1), positive='TRUE')
 
-TruePos <- list()
-TrueNeg <- list()
+SensitivityTruePos <- list()
+OneMinusSpecificityFalsePositive <- list()
 p = .1
 while (p < 1){
   cm2 <- confusionMatrix(table(predict(M_LOG2, Valid, type="response") >= p,
                               Valid$loan_status == 1), positive='TRUE')
   byclass.minTruePos <- cm2$byClass['Sensitivity']
-  byclass.minFalsePos <- cm2$byClass['Neg Pred Value']
-  TruePos <- append(TruePos,byclass.minTruePos) 
-  TrueNeg <- append(TrueNeg,byclass.minFalsePos) 
+  byclass.minFalsePos <- (1 - cm2$byClass['Specificity'])
+  SensitivityTruePos <- append(SensitivityTruePos,byclass.minTruePos) 
+  OneMinusSpecificityFalsePositive <- append(OneMinusSpecificityFalsePositive,byclass.minFalsePos) 
+  print(byclass.minTruePos)
   print(byclass.minFalsePos)
   p <- p + .1
 }
+##p = .3 has Sensitivity of 0.7049892 and 1-Specificity of 0.1297857 
 
 ##Attempt at ROC plot
-plot(TrueNeg,TruePos, ylim = 0:1, xlim = 0:1)
+plot(OneMinusSpecificityFalsePositive,SensitivityTruePos)
 
 
+##Addition models: CART, Random Forest, SVM
 
+#####################
+########CART#########
+#####################
 
+#rpart package implementation
+train_control <- trainControl(method="cv", number=10, savePredictions = TRUE)
+M_CART <- train(loan_status ~., data = Train, trControl=train_control, tuneLength=10, method = "rpart", na.action = na.omit) #increasing tunelength increases regularization penalty
+##the "cv", number = 10 refers to 10-fold cross validation on the training data
+plot(M_CART) #produces plot of cross-validation results
+M_CART$bestTune #returns optimal complexity parameter
+confusionMatrix(predict(M_CART, Valid), Valid$loan_status, positive='1')
